073c056a13d12661eac80d518adfb31f1b4dc4e1
Scooter <martinkos007@gmail.com>
2019-05-28 11:58:28 +0200

    update

--- Meteo project/master/SELECT.sql
+++
@@ -1,1 +0,0 @@
-select distinct sigfox_id, sensor_id, point_id, max(date) from master_log ml1 where date > CURRENT_DATE - INTERVAL '1 month' group by sigfox_id, sensor_id, point_id order by point_id;
