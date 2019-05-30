010123b6a00814e8a05d4f02cf62fcb55f96280c
Scooter <martinkos007@gmail.com>
2019-05-31 00:10:27 +0200

    Commit

--- Java Script/Circle without libary/index.js
+++ Java Script/Circle without libary/index.js
@@ -4,7 +4,6 @@ function onLoad() {
   c = document.getElementById("myCanvas");
   ctx = c.getContext("2d");
   ctx.moveTo(0, 0);
-  ctx.stroke();
   for (let i = 0; i < 360 * 2; i++) {
     drawPixel(
       Math.sin(i / 2.0) * 50 + c.width / 2,
@@ -12,6 +11,9 @@ function onLoad() {
       ctx
     );
   }
+  ctx.beginPath();
+  ctx.arc(c.width / 2, c.height / 2, 55, 0, 2 * Math.PI);
+  ctx.stroke();
 }
 function drawPixel(x, y, ctx) {
   ctx.fillRect(x, y, 1, 1);
